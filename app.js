9dd1ccaf4d83b5383864e133eecab7698a25243a
Vitaliy Ivanov <wicharek@gmail.com>
2016-01-21 17:06:08 +0200

    Model and entities basically implemented

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var restify = require('restify');
 var nconf = require('nconf');
+var promise = require('promise');
 
 var ModelDefinition = require('./lib/model/definition/ModelDefinition.js')
 var SequelizeModel = require('./lib/model/sequelize/SequelizeModel.js')
+var backer = require('./lib/model/Backer.js');
 
 nconf.argv().env('_');
 
@@ -16,6 +18,22 @@ var modelData = require(nconf.get('paths:model'));
 var modelDefinition = new ModelDefinition(modelData);
 
 var model = new SequelizeModel();
-model.define(modelDefinition, nconf.get('database:uri'), nconf.get('database:options')).then(function() {
+model.define(modelDefinition, promise, nconf.get('database:uri'), nconf.get('database:options')).then(function() {
     console.log('Model setup completed');
+
+    var Backer = backer(model, promise);
+    
+    Backer.Artist.create({
+        title: 'The Killers',
+        published: true
+    }).save().then(function(artist) {
+        return Backer.Artist.get('1');
+    }).then(function(artist) {
+        console.log(artist);
+        return Backer.Artist.query().where({ title: 'The Killers' }).find();
+    }).then(function(artists) {
+        console.log(artists);
+    }, function(error) {
+        console.log(error);
+    });
 });
