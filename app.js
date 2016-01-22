329cf4e06cdded6fe6055687c765189871787ec5
Vitaliy Ivanov <wicharek@gmail.com>
2016-01-22 19:57:00 +0200

    Support for working with many-to-X relations

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var restify = require('restify');
 var nconf = require('nconf');
 var promise = require('promise');
 
+require('promise/lib/rejection-tracking').enable();
+
 var ModelDefinition = require('./lib/model/definition/ModelDefinition.js')
 var SequelizeModel = require('./lib/model/sequelize/SequelizeModel.js')
 var backer = require('./lib/model/Backer.js');
@@ -28,26 +30,44 @@ model.define(modelDefinition, promise, nconf.get('database:uri'), nconf.get('dat
         published: true
     });
     
-    var genre = Backer.Genre.create({
-        title: 'Dance Rock'
+    album.save().then(function() {
+        return Backer.Promise.all([
+            Backer.Track.create({ title: 'Tranquilize (Feat. Lou Reed)' }).save(),
+            Backer.Track.create({ title: 'Shadowplay' }).save(),
+            Backer.Track.create({ title: 'All the Pretty Faces' }).save()
+        ]);
+    }).then(function(tracks) {
+        return album.relation('tracks').add(tracks);
+    }).then(function() {
+        return Backer.Album.get('1');
+    }).then(function(albumAgain) {
+        return albumAgain.relation('tracks').fetch(2);
+    }).then(function(tracksAgain) {
+        return album.relation('tracks').remove(tracksAgain);
+    }).then(function(tracks) {
+        console.log('ok');
     });
     
-    genre.save().then(function() {
-        album.set('genre', genre);
-        console.log(album.get('genre').get('title'));
-        
-        return album.save();
-    }).then(function() {
-        return Backer.Album.query()
-            .where({ id: '1' })
-            .include(['genre'])
-            .find();
-    }).then(function(albums) {
-        console.log(albums[0].get('genre').get('title'));
-        return Backer.Promise.resolve();
-    }).then(function() {
-        console.log('ok');
-    }, function(error) {
-        console.log(error);
-    });
+    // var genre = Backer.Genre.create({
+    //     title: 'Dance Rock'
+    // });
+    //
+    // genre.save().then(function() {
+    //     album.set('genre', genre);
+    //     console.log(album.get('genre').get('title'));
+    //
+    //     return album.save();
+    // }).then(function() {
+    //     return Backer.Album.query()
+    //         .where({ id: '1' })
+    //         .include(['genre'])
+    //         .find();
+    // }).then(function(albums) {
+    //     console.log(albums[0].get('genre').get('title'));
+    //     return Backer.Promise.resolve();
+    // }).then(function() {
+    //     console.log('ok');
+    // }, function(error) {
+    //     console.log(error);
+    // });
 });
